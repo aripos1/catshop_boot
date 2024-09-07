fa3dbec46a08004e478c0c13a3280bb611634bf3
soyoungJin44 <wlsthdud0412@naver.com>
2024-09-07 23:51:56 +0900

    1315

--- src/main/java/com/javaex/controller/JUserController.java
+++ src/main/java/com/javaex/controller/JUserController.java
@@ -1,11 +1,24 @@
 package com.javaex.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.javaex.service.JUserService;
 
 @Controller
 public class JUserController {
 
-	private JUserService juserService;      
+	private JUserService juserService;
+	
+	// 리스트 출력준비
+	@RequestMapping(value="/user/list", method= {RequestMethod.GET, RequestMethod.POST})
+	public String list() {
+		System.out.println("list j w");
+		
+		
+		
+		
+		return "";
+	}
 }
