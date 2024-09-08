16b1cc5d101fb090deea3063402f63e32b126505
Hwangduckrong <hwangsee12@naver.com>
2024-09-08 10:55:52 +0900

    2024-09-08

--- src/main/java/com/javaex/controller/DUserController.java
+++ src/main/java/com/javaex/controller/DUserController.java
@@ -1,11 +1,17 @@
 package com.javaex.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
 public class DUserController {
-	/*111111*/
-
 	
+	@RequestMapping(value="/user/join", method= {RequestMethod.GET, RequestMethod.POST})
+	public String join() {
+		System.out.println("ready to join");
+
+		return "";
+	}
 }
