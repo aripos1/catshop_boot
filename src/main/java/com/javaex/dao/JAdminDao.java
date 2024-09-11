7388578563e72ef45931962b0a150f3666e87175
Jimin-96 <wlalsdl684@naver.com>
2024-09-11 09:23:56 +0900

    연결 완

--- src/main/java/com/javaex/dao/JAdminDao.java
+++ src/main/java/com/javaex/dao/JAdminDao.java
@@ -65,7 +65,6 @@ public class JAdminDao {
 	
 	public List<JOptionVo> optionselectlist(int no){
 		System.out.println("optionlist dao j w");
-		
 		List<JOptionVo> optionList = sqlSession.selectList("admingoodslist.optionnumlist",no);
 		System.out.println(optionList);
 		
